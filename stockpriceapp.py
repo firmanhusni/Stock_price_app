50201adfe05d41c5866740a8afb199e8429a76b5
firmanhusni <43990513+firmanhusni@users.noreply.github.com>
2021-03-30 19:46:09 +0200

    added text box for stock ticker

--- stockpriceapp.py
+++ stockpriceapp.py
@@ -5,11 +5,12 @@ import pandas as pd
 st.write("""
 # Simple Stock Price App
 
-Shown are the stock closing price and volume of Tesla!
+Shown are the stock closing price and volume of selected company!
 
 """)
+symbol = st.text_input('Please input the ticker symbol','TSLA')
 
-tickerSymbol = 'TSLA'
+tickerSymbol = symbol
 tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 
